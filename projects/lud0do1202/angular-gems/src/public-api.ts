57885475c5ee93893bfb25e1a5160080450d958e
Lud0do1202 <ludo.traina@gmail.com>
2023-10-17 11:20:38 +0200

    update path search-bar

--- projects/lud0do1202/angular-gems/src/public-api.ts
+++ projects/lud0do1202/angular-gems/src/public-api.ts
@@ -1,3 +1,4 @@
 export * from './lib/components/menu/menu-x';
 export * from './lib/components/menu/menu-y-icons';
+export * from './lib/components/search/search-bar';
 export * from './lib/angular-gems.module';
